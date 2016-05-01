1788e2e7ffd7982e6c3142995d4b0c91c2a7b9be
DominikaBogusz <dominika.bogu.bogusz@gmail.com>
2016-05-01 10:30:56 +0200

    Question UI improved

--- Assets/QuestionPoint.cs
+++ Assets/QuestionPoint.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class QuestionPoint : MonoBehaviour {
 
@@ -7,11 +8,55 @@ public class QuestionPoint : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
     {
+        //pausing the game
+        Time.timeScale = 0.0f;
+
         questionCanvas.SetActive(true);
     }
 
-    public void Answer()
+    public void Check()
     {
-        questionCanvas.SetActive(false);
+        Transform question = questionCanvas.transform.Find("Question");
+        Toggle a = question.Find("A").GetComponent<Toggle>();
+        Toggle b = question.Find("B").GetComponent<Toggle>();
+        Toggle c = question.Find("C").GetComponent<Toggle>();
+        Toggle d = question.Find("D").GetComponent<Toggle>();
+
+        if (a.isOn)
+        {
+            a.GetComponentInChildren<Text>().color = Color.red;
+        }
+
+        if (b.isOn)
+        {
+            b.GetComponentInChildren<Text>().color = Color.green;
+        }
+
+        if (c.isOn)
+        {
+            c.GetComponentInChildren<Text>().color = Color.red;
+        }
+
+        if (d.isOn)
+        {
+            d.GetComponentInChildren<Text>().color = Color.red;
+        }
+
+        a.interactable = false;
+        b.interactable = false;
+        c.interactable = false;
+        d.interactable = false;
+
+        Button button = questionCanvas.transform.Find("Button").GetComponent<Button>();
+        button.GetComponentInChildren<Text>().text = "Graj dalej";
+        button.onClick.AddListener(() => GoBack());
+    }
+
+    public void GoBack()
+    {
+        //unpausing the game
+        Time.timeScale = 1.0f;
+
+        Destroy(this.gameObject);
     }
 }
